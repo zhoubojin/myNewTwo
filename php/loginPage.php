d1a35bf9e6db852f9377fa027d2fe3b8546affbd
Your Name <email@example.com>
2017-09-06 11:07:06 +0800

    绝版2

--- php/loginPage.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$userName = $_POST['userNa'];
-	$userPass = $_POST['userPa'];
-	if($userName=="931015811@163.com" && $userPass=="w12345"){
-		echo "1";
-	}else{
-		echo "0";
-	}
-?>
--- php/registrationPageTwo.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$userName = $_GET['dzyj1'];
-	if($userName=="931015811@163.com"){
-		echo "1";
-	}else{
-		echo "0";
-	}
-?>
